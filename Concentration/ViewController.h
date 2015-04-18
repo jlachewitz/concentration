a709da667259e6e513f90b18d52150a4cdb05d96
Jessica Lachewitz <jlachewitz@PC101564-2.home>
2015-04-18 11:53:27 -0400

    Initial Commit

---
+++ Concentration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Concentration
+//
+//  Created by Jessica Lachewitz on 4/18/15.
+//  Copyright (c) 2015 Jessica Lachewitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Concentration/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Concentration
+//
+//  Created by Jessica Lachewitz on 4/18/15.
+//  Copyright (c) 2015 Jessica Lachewitz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
